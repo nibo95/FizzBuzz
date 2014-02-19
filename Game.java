b9b55f64d54e1a3611de5ba14d71866f79ddcf58
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-02-19 17:40:53 -0500

    initial commit

---
+++ Game.java
@@ -0,0 +1,26 @@
+
+public class Game 
+{
+	public void fizzBuzz(int gameSize)
+	{
+		for (int i=1; i<=gameSize; i++)
+		{
+			if (i>1)
+				System.out.print(", ");
+			if ((i%3 == 0) && (i%5 == 0))
+				System.out.print("fizzbuzz");
+			else if (i%3 == 0)
+				System.out.print("fizz");
+			else if (i%5 == 0)
+				System.out.print("buzz");
+			else
+				System.out.print(i);
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Game first = new Game();
+//		first.fizzBuzz(enter length of game here);
+	}
+
+}
